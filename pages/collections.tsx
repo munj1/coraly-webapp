963b3116b985ebda6069a9875a9c1af925531371
munj1 <yimunj931@gmail.com>
2022-11-01 19:44:25 +0900

    dd

---
+++ pages/collections.tsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/navwallet/Navbar";
+import { Box } from "@chakra-ui/react";
+
+const CollectionsPage = () => {
+  return (
+    <Box w={"full"}>
+      <Navbar />
+    </Box>
+  );
+};
+
+export default CollectionsPage;
