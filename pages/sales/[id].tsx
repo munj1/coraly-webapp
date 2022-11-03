9acc50314347397329e22fbbb806bbfa6886b103
munj1 <yimunj931@gmail.com>
2022-11-03 13:47:43 +0900

    change paper.xyz link

--- pages/sales/[id].tsx
+++ pages/sales/[id].tsx
@@ -120,14 +120,13 @@ const DetailPage = ({ nft, share, id, condition }) => {
             <Button onClick={handleClaim} isLoading={isClaiming}>
               Buy
             </Button>
-            {id === "0" && (
+            {id === "2" && (
               <>
-                <Link href="https://paper.xyz/checkout/b8b5a26e-1840-44ac-aa1e-5218c4e07577">
+                <Link href="https://paper.xyz/checkout/82bd6b05-cb39-4a3f-9a38-63b365bea77a">
                   <Button>Pay with Credit Card</Button>
                 </Link>
                 <Text size="xs">
-                  카드결제는 paper.xyz 이용한거임 링크를 따로 생성해서 db에
-                  저장해야함
+                  카드결제 - paper.xyz 링크를 따로 생성해서 db에 저장해야함
                 </Text>
               </>
             )}
